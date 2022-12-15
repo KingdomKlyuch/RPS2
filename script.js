4c329247a9212b51dc8fe10c6ec3d9c9432b7236
KingdomKlyuch <kneedy93@gmail.com>
2022-12-15 10:06:36 +0200

    Query select HTML elements for RPS game

---
+++ script.js
@@ -0,0 +1,7 @@
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissors = document.querySelector('.scissors')
+const winner = document.querySelector('.winner')
+const playerScore = document.querySelector('.playerScore')
+const computerScore = document.querySelector('.computerScore')
+const returnResult = document.querySelector('.returnResult')
