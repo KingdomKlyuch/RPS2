29c6986d74aac5a860dd1db5157d2f6a5ea747e5
KingdomKlyuch <kneedy93@gmail.com>
2022-12-15 10:14:42 +0200

    Pseudocode

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ const winner = document.querySelector('.winner')
 const playerScore = document.querySelector('.playerScore')
 const computerScore = document.querySelector('.computerScore')
 const returnResult = document.querySelector('.returnResult')
+
+// computer generate RPS using math random and return
+
+
+
+//playround function that uses result to return the game results from else if statements and makes changes to player score
+
+
+
+
+
+
+//player win
+
+
+
+
+//computer win
+
+
+
+
+// display player choice and computer choice
+
+
+
+
+//if computer or player wins, game over
