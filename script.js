53458b1ba6b98ffec163f09b381adeb213f5b59a
KingdomKlyuch <kneedy93@gmail.com>
2022-12-15 10:31:08 +0200

    Write game logic, win conditions, scoring

--- script.js
+++ script.js
@@ -12,21 +12,40 @@ const getComputerChoice = () => {
     return choices[Math.floor(Math.random() * choices.length)];
   };
   
-
-
 //playround function that uses result to return the game results from else if statements and makes changes to player score
-
-
-
-
-
-
-//player win
-
-
-
-
-//computer win
+const playRound = (playerSelection) => {
+    const computerSelection = getComputerChoice();
+    
+    // Display player and computer choices
+    playerScore.innerHTML = `Player: ${playerSelection}`;
+    computerScore.innerHTML = `Computer: ${computerSelection}`;
+    
+    // Compare player and computer choices and award a point to the winner
+    if (playerSelection === computerSelection) {
+      // Draw
+      returnResult.innerHTML = "It's a draw!";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      // Player wins
+      playerScore.innerHTML = `Player: ${playerSelection} (+1 point)`;
+      returnResult.innerHTML = "Player wins!";
+      playerScore += 1;
+      if (playerScore === 5) {
+        winner.innerHTML = "Game over: Player wins!";
+      }
+    } else {
+      // Computer wins
+      computerScore.innerHTML = `Computer: ${computerSelection} (+1 point)`;
+      returnResult.innerHTML = "Computer wins!";
+      computerScore += 1;
+      if (computerScore === 5) {
+        winner.innerHTML = "Game over: Computer wins!";
+      }
+    }
+  };
 
 
 
